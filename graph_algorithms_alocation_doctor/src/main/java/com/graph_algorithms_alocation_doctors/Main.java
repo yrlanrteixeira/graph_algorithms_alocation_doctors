4547cfb5d379ce1cfbe481a991c7faebd6fbc71b
Yrlan R Teixeira <yrlan.01@hotmail.com>
2024-06-17 19:50:26 -0300

    Initial commit

---
+++ graph_algorithms_alocation_doctor/src/main/java/com/graph_algorithms_alocation_doctors/Main.java
@@ -0,0 +1,9 @@
+package main.java.com.graph_algorithms_alocation_doctors;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new DoctorSchedulingGUI());
+    }
+}
